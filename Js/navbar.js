b7c103a4ae0918eb765a2ff6718b4060ee5ab385
sherifemad10 <sherifemad.web@gmail.com>
2025-03-08 00:31:41 +0200

    nav responsive

---
+++ Js/navbar.js
@@ -0,0 +1,11 @@
+let nav = document.getElementById("menu");
+let search = document.querySelector(".form");
+
+
+function toggleNavbar() {
+  nav.classList.toggle("show");
+}
+
+function toggleSearch() {
+  search.classList.toggle("show");
+}
