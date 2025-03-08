618e6a1278d60d3fdafdda039b872fb7b7fa4e20
sherifemad10 <sherifemad.web@gmail.com>
2025-03-09 00:42:10 +0200

    home prodect

--- Js/homeProdect.js
+++ Js/homeProdect.js
@@ -1,20 +1,25 @@
-
 let newArrivalsContainer = document.querySelector(".new-arrivals-contant");
 let topSellingContainer = document.querySelector(".topSelling-contant");
 
 function newArrivals() {
   fetch("./data/prodects.json")
-    .then(response => response.json())
-    .then(data => {
-      let arrivals = data.filter(product => product.newArrivals === true);
+    .then((response) => response.json())
+    .then((data) => {
+      let arrivals = data.filter((product) => product.newArrivals === true);
 
       // console.log(arrivals);
 
-      newArrivalsContainer.innerHTML = arrivals.slice(0,4).map(item => `
+      newArrivalsContainer.innerHTML = arrivals
+        .slice(0, 4)
+        .map(
+          (item) => `
       <div class="new-arrivals-card">
 
           <figure class="prodect-img">
-            <img src="${item.images[0].url}" alt="new-arrivals-1">
+            <img src="${item.images[0].url}" alt=${item.name}
+            loading="lazy"
+
+            >
           </figure>
           <h3>${item.name}</h3>
           <div class="rating">
@@ -28,34 +33,43 @@ function newArrivals() {
 
             ${item.oldPrice ? `<del class="old">$${item.oldPrice}</del>` : ""}
 
-                        ${item.discount ? `<span class="discount">-${item.discount}%</span>` : ""}
+              ${
+                          item.discount
+                            ? `<span class="discount">-${item.discount}%</span>`
+                            : ""
+                        }
 
             
           </div>
           
 
         </div>
-      `).join(""); 
+      `
+        )
+        .join("");
     })
-    .catch(error => console.error("Error:", error));
+    .catch((error) => console.error("Error:", error));
 }
 
 newArrivals();
 
-
 function topSelling() {
   fetch("./data/prodects.json")
-    .then(response => response.json())
-    .then(data => {
-      let topSelling = data.filter(product => product.topSelling === true);
+    .then((response) => response.json())
+    .then((data) => {
+      let topSelling = data.filter((product) => product.topSelling === true);
       // console.log(topSelling)
 
-
-      topSellingContainer.innerHTML = topSelling.slice(0,4).map(item => `
+      topSellingContainer.innerHTML = topSelling
+        .slice(0, 4)
+        .map(
+          (item) => `
       <div class="new-arrivals-card">
 
           <figure class="prodect-img">
-            <img src="${item.images[0].url}" alt="new-arrivals-1">
+            <img src="${item.images[0].url}" alt=${item.name}
+            loading="lazy"
+            >
           </figure>
           <h3>${item.name}</h3>
           <div class="rating">
@@ -69,16 +83,22 @@ function topSelling() {
 
             ${item.oldPrice ? `<del class="old">$${item.oldPrice}</del>` : ""}
 
-                        ${item.discount ? `<span class="discount">-${item.discount}%</span>` : ""}
+                        ${
+                          item.discount
+                            ? `<span class="discount">-${item.discount}%</span>`
+                            : ""
+                        }
 
             
           </div>
           
 
         </div>
-      `).join(""); 
+      `
+        )
+        .join("");
     })
-    .catch(error => console.error("Error:", error));
+    .catch((error) => console.error("Error:", error));
 }
 
 topSelling();
